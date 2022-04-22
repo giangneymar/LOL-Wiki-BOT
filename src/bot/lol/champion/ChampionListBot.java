427b7c6a9b89d314a1cd834a692a45849249f2ba
buidat <99buidat@gmail.com>
2022-04-22 18:00:47 +0700

    dat: update champion & add champion_list commit 2

---
+++ src/bot/lol/champion/ChampionListBot.java
@@ -0,0 +1,70 @@
+package bot.lol.champion;
+
+import bot.BaseBot;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import storage.AppStorage;
+import storage.models.database.Database;
+import utils.AppLogger;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class ChampionListBot extends BaseBot {
+    private AppLogger appLogger = AppLogger.getInstance();
+    private AppStorage appStorage = AppStorage.getInstance();
+
+    public ChampionListBot(int maxThread, long restTime) {
+        super(maxThread, restTime);
+        toolkit.appLogger.info("create");
+    }
+
+    public void getListChampion() {
+        executor.execute(() -> {
+            try {
+                toolkit.appLogger.info("start crawl");
+                toolkit.appLogger.info("crawl allChampion");
+                crawlListChampion("https://www.leagueoflegends.com/en-us/champions/");
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+
+    public void crawlListChampion(String link) throws IOException, SQLException {
+        ChampionBotDatabase championBotDatabase = new ChampionBotDatabase();
+        Database database = appStorage.config.database;
+        championBotDatabase.delete(database.table.champion_list);
+        String imageChamp, name;
+        int idChampion;
+
+        Document document = Jsoup.connect(link).get();
+        Elements elementsListChampOfLink = document.select("div.style__List-sc-13btjky-2.dLJiol a");
+        int i = 0;
+        for (Element elementsChampOfLink : elementsListChampOfLink) {
+            idChampion = i;
+            appLogger.info(String.format("get image champion %s", i));
+            Elements elementsImageChamp = elementsChampOfLink.getElementsByTag("img");
+            imageChamp = elementsImageChamp.first().absUrl("src");
+            appLogger.info(String.format("get name champion %s", i));
+            Elements elementsNameChampion = elementsChampOfLink.select("span.style__Text-n3ovyt-3.gMLOLF");
+            name = elementsNameChampion.first().text();
+            championBotDatabase.insertListChampion(database.table.champion_list, idChampion, name, imageChamp);
+            i++;
+        }
+        appLogger.info(String.format("list champion has %s item", i));
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        toolkit.appLogger.info("prepare");
+        getListChampion();
+        complete();
+    }
+}
